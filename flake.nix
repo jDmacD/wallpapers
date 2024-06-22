2c5f801f97c4260f66faff7567fc3cc75cdc8d83
jDmacD <jmacdoo@gmail.com>
2024-06-22 12:12:33 +0100

    rework

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs } @ inputs: let
+  outputs = {
+    self,
+    nixpkgs,
+  } @ inputs: let
     inherit (self) outputs;
     systems = [
       "aarch64-linux"
@@ -15,23 +18,27 @@
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     packages = forAllSystems (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+      pkgs = import nixpkgs {inherit system;};
+    in {
       wallpapers = pkgs.stdenv.mkDerivation {
         name = "wallpapers";
         src = ./.;
-        buildInputs = [ pkgs.coreutils pkgs.bash ];
+        buildInputs = [pkgs.coreutils pkgs.bash];
 
         installPhase = ''
-          mkdir -p $out/share/wallpapers/3440x1440
-          mkdir -p $out/bin
+          mkdir -p $out/share/wallpapers
           cp -r $src/3440x1440/* $out/share/wallpapers/3440x1440/
+          mkdir -p $out/bin
+
+          random_image=$(find $out/share/wallpapers/3440x1440 -type f -name '*.jpeg' | shuf -n 1)
+
           cat > $out/bin/random-wallpaper <<EOF
           #!/usr/bin/env bash
           find $out/share/wallpapers/3440x1440 -type f -name '*.jpg' | shuf -n 1
           EOF
           chmod +x $out/bin/random-wallpaper
+
+          echo $random_image > $out/share/wallpapers/random-image
         '';
 
         meta = with pkgs.lib; {
@@ -39,11 +46,20 @@
           license = licenses.mit;
         };
       };
+
+      randomImage = pkgs.writeShellScriptBin "random-image" ''
+        cat ${self.packages.${system}.wallpapers}/share/wallpapers/random-image
+      '';
     });
 
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    nixosModules.wallpapers = { config, lib, pkgs, ... }: {
+    nixosModules.wallpapers = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: {
       options.wallpapers = {
         enable = lib.mkOption {
           type = lib.types.bool;
@@ -55,14 +71,29 @@
           default = pkgs.wallpapers;
           description = "The package that contains the wallpapers";
         };
+        randomImage = lib.mkOption {
+          type = lib.types.str;
+          default = "${pkgs.wallpapers}/share/wallpapers/random-image";
+          description = "Path to the randomly selected wallpaper";
+        };
       };
 
       config = lib.mkIf config.wallpapers.enable {
-        environment.systemPackages = [ config.wallpapers.package ];
+        environment.systemPackages = [config.wallpapers.package];
         environment.etc."wallpapers".source = "${config.wallpapers.package}/share/wallpapers";
       };
     };
 
     defaultPackage = forAllSystems (system: self.packages.${system}.wallpapers);
+
+    apps = forAllSystems (system: {
+      randomImage = {
+        type = "app";
+        program = "${self.packages.${system}.randomImage}/bin/random-image";
+      };
+    });
+
+    # Set default app
+    defaultApp = forAllSystems (system: self.apps.${system}.randomImage);
   };
 }
